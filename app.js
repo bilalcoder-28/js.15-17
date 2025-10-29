eb73773980e0a6bf0425b5bc46221694e568114b
bilalcoder-28 <bilalasif2007@gamil.com>
2025-10-30 03:36:01 +0500

    helllllllllooooooo

---
+++ app.js
@@ -0,0 +1,70 @@
+// Q no 3
+// A no 3
+
+
+let a =["Bilal","ismail", "asif","amir","mirza galib"]
+console.log(a)
+
+//Q no 4
+//A no 4
+
+let b =[1,2,3,4,5,5,6,7,9,90,]
+console.log(b)
+
+
+//Q no 5
+//A no 5
+
+let c = [true,false,]
+console.log(c)
+
+
+//Q no 6
+//A no 6 
+
+let d = [1,"bilal",true]
+console.log(d)
+
+//Q no 7
+//A no 7 
+
+let edu = ["SSC","HSC","PHD","BS","BSC","BCOM","MS"]
+console.log(edu)
+document.writeln("<h2>Education!!</h2>")
+document.writeln(edu[0])
+document.writeln(edu[1])
+document.writeln(edu[2])
+document.writeln(edu[3])
+document.writeln(edu[4])
+document.writeln(edu[5])
+document.writeln(edu[6])
+
+
+
+//Q no 8
+//A no 8
+
+let stu = ["bilal","sufiyan","arham","amir"]
+let score = [430,450,490,240]
+let totalmarks = 500
+
+console.log(`Score of Student ${stu[0]} is ${score[0]}.persentage of ${(score[0] / totalmarks)*100}% `)
+console.log(`Score of Student ${stu[1]} is ${score[2]}.persentage of ${(score[1] / totalmarks)*100}% `)
+console.log(`Score of Student ${stu[2]} is ${score[2]}.persentage of ${(score[2] / totalmarks)*100}% `)
+console.log(`Score of Student ${stu[3]} is ${score[3]}.persentage of ${(score[3] / totalmarks)*100}% `)
+
+document.writeln(`<p>Score of Student ${stu[0]} is ${score[0]}.persentage of ${(score[0] / totalmarks)*100}%</p>`)
+document.writeln(`<p>Score of Student ${stu[1]} is ${score[2]}.persentage of ${(score[1] / totalmarks)*100}%</p>`)
+document.writeln(`<p>Score of Student ${stu[2]} is ${score[2]}.persentage of ${(score[2] / totalmarks)*100}%</p>`)
+document.writeln(`<p>Score of Student ${stu[3]} is ${score[3]}.persentage of ${(score[3] / totalmarks)*100}%</p>`)
+
+
+//Q no 11
+//A no 11
+let cities = ["Karachi","Lahore","Quetta","Peshawar"]
+console.log(cities);
+
+let copycities= cities.slice(0,2) 
+console.log( "Copy Cities--->",copycities);
+
+
